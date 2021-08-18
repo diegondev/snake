abab895635960afabd14142e782b041f7a7ddd3e
Diego Nascimento <diegohfn@gmail.com>
2021-08-18 12:27:13 -0400

    Sets game over conditions

--- js/script.js
+++ js/script.js
@@ -36,6 +36,13 @@ function iniciarJogo() {
     if (snake[0].y > 15 * box && direction === 'down') snake[0].y = 0;
     if (snake[0].y < 0 && direction === 'up') snake[0].y = 16 * box;
 
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
+            clearInterval(jogo);
+            alert('Game Over :(')
+        }
+    }
+
     criarBG();
     criarCobrinha();
     criaComida();
@@ -72,4 +79,4 @@ function update(event) {
     if (event.keyCode === 40 && direction != 'up') direction = 'down';
 }
 
-let jogo = setInterval(iniciarJogo, 50);
+let jogo = setInterval(iniciarJogo, 100);
